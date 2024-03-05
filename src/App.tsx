1d61719546628c399936d74ec5a644a6c8085c8c
Francis Angoring <angoringfrancis29@gmail.com>
2024-03-05 12:26:54 +0800

    Added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import CatCareSection from "./components/CatCareSection";
 import Hero from "./components/Hero";
-import Hero2 from "./components/CatCareService";
 import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
 import Family from "./components/Family";
 import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <CatCareSection />
-      <Hero2 />
-      <Pricing />
-      <Family />
-      <Newsletter />
+      <div className="container mx-auto">
+        <Navbar />
+        <Hero />
+        <CatCareSection />
+        <Pricing />
+        <Family />
+        <Newsletter />
+      </div>
+      <Footer />
     </>
   );
 }
