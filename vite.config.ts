ef197d3eb50cc8c53cfd232d446e34692c87dd7f
Ambooon <angoringfrancis29@gmail.com>
2024-08-01 15:32:37 +0800

    Install gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Gentle-Paws-Cat-Care/",
+  base: '/Gentle-Paws-Cat-Care/',
   plugins: [react()],
 })
